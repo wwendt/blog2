952b941434eac8293bc9f29245becd37a3c23b50
Wes <westin.wendt2@gmail.com>
2017-02-08 17:59:51 -0800

    message

---
+++ blog2.py
@@ -0,0 +1,75 @@
+import webapp2
+import os
+import re
+import jinja2
+from string import letters
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+	def get(self):
+		self.write("hello world!")
+
+#blog stuff below
+
+def blog_key(name = 'default'):
+	return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+	subject = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
+
+	def render(self):
+		self._render_text = self.content.replace('\n', '<br>')
+		return render_str("post.html", p = self)
+class BlogFront(Handler):
+	def get(self):
+		posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 10")
+		self.render('front.html', posts = posts)
+
+class PostPage(Handler):
+	def get(self, post_id):
+		key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+		post = db.get(key)
+
+		if not post:
+			self.error(404)
+			return
+		self.render("permalink.html", post = post)
+
+class NewPost(Handler):
+	def get(self):
+		self.render("newpost.html")
+
+	def post(self):
+		subject = self.request.get('subject')
+		content = self.request.get('content')
+
+		if subject and content:
+			p = Post(parent = blog_key(), subject = subject, content = content)
+			p.put()
+			self.redirect('/blog/%s' % str(p.key().id()))
+		else:
+			error = "subject and content, please"
+			self.render("newpost.html", subject=subject, content=content, error=error)
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/front.html', BlogFront), ('/permalink.html', PostPage), ('/newpost.html', NewPost)], debug=True)
+
